fa23db5cf8751bcef0b7f351b5efda136e5a737e
ykadec <ykadec2016@kellogg.northwestern.edu>
2015-06-02 16:36:33 -0500

    after adding user table and some changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602191847) do
+ActiveRecord::Schema.define(version: 20150602210959) do
 
   create_table "beers", force: :cascade do |t|
     t.text     "production_location"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20150602191847) do
   end
 
   create_table "new_entries", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
